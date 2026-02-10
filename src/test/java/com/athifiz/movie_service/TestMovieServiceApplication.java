16259fadb048a93d74f78470818d0dc0d8937af7
AthiFiz <athif.rasheedh@gmail.com>
2026-02-10 20:00:17 +0530

    first commit

---
+++ src/test/java/com/athifiz/movie_service/TestMovieServiceApplication.java
@@ -0,0 +1,11 @@
+package com.athifiz.movie_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestMovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(MovieServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
