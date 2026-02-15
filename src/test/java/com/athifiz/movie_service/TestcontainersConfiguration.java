47854a355f2715edf8855050ca60e3a4c60f4bcb
AthiFiz <athif.rasheedh@gmail.com>
2026-02-15 13:49:56 +0530

    change the db to mysql

--- src/test/java/com/athifiz/movie_service/TestcontainersConfiguration.java
+++ src/test/java/com/athifiz/movie_service/TestcontainersConfiguration.java
@@ -3,7 +3,7 @@ package com.athifiz.movie_service;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.mysql.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -11,8 +11,8 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+	MySQLContainer mySQLContainer() {
+		return new MySQLContainer(DockerImageName.parse("mysql:latest"));
 	}
 
 }
